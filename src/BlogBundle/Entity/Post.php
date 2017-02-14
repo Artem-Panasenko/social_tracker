8ee6523a1cf813c6e420271f1820bec8cae9c920
dmitryChernukh <dmitrychernukh@gmail.com>
2017-02-15 01:17:48 +0200

    Added new bundle - Blog

---
+++ src/BlogBundle/Entity/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraint as Assert;
+
+/**
+ * Class Post
+ * @package BlogBundle\Entity
+ * @ORM\Table(name="blog_post")
+ */
+class Post
+{
+
+}
